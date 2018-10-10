32cf7b55f28215d27f1fb208d39bf42851a64741
hrabovszki2310 <zh@openkeyword.de>
2018-10-10 07:17:27 +0200

    create frames "frm4T_Clone" and "frm4T_Login"

---
+++ Test44Test/src/test/java/okw/gui/frames/frm4T_Clone.java
@@ -0,0 +1,13 @@
+package okw.gui.frames;
+
+import okw.gui.adapter.selenium.SeBrowserChild;
+
+public class frm4T_Clone extends SeBrowserChild
+{
+
+    public frm4T_Clone()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ Test44Test/src/test/java/okw/gui/frames/frm4T_Login.java
@@ -0,0 +1,23 @@
+package okw.gui.frames;
+
+import okw.OKW;
+import okw.gui.adapter.selenium.*;
+
+
+/**
+ * https://4test.io:10443/login
+ * 
+ * @author zoltan
+ */
+@OKW( FN = "4Test Login" )
+public class frm4T_Login extends SeBrowserChild
+{
+    @OKW( FN = "Sign in with Google" )
+    public SeButton GoogleSignin = new SeButton("//button[contains(@class, 'googlesignin')]"); 
+    
+    public frm4T_Login()
+    {
+        setLocator( "//*[contains(@class, 'loginArea')]/../../.." );
+    }
+
+}
